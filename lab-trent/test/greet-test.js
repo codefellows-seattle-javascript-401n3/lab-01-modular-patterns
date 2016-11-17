ee2d9f8cda3a7896bf9919697976a1f7153910bb
Trenton Kress <trenton.kress@gmail.com>
2016-11-16 17:40:09 -0800

    Added another describe to the test to be more specific about which function is being tested.

--- lab-trent/test/greet-test.js
+++ lab-trent/test/greet-test.js
@@ -2,10 +2,12 @@ const assert = require('assert');
 const greet = require('../greet');
 
 describe('Greet', function() {
-  it('should greet the user', function() {
-    assert.equal(greet.greet(), 'hello');
-  });
-  it('should greet the user by the username passed in as a parameter', function() {
-    assert.equal(greet.greet('tom'), 'hello tom');
+  describe('greet()', function() {
+    it('should greet the user', function() {
+      assert.equal(greet.greet(), 'hello');
+    });
+    it('should greet the user by the username passed in as a parameter', function() {
+      assert.equal(greet.greet('tom'), 'hello tom');
+    });
   });
 });
