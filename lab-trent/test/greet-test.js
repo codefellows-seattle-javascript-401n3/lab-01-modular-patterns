3a826181a5b6291e379e7119081794caefb1a5ba
Trenton Kress <trenton.kress@gmail.com>
2016-11-16 17:36:47 -0800

    Edited the package json and added the greet module with a couple tests.

---
+++ lab-trent/test/greet-test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const greet = require('../greet');
+
+describe('Greet', function() {
+  it('should greet the user', function() {
+    assert.equal(greet.greet(), 'hello');
+  });
+  it('should greet the user by the username passed in as a parameter', function() {
+    assert.equal(greet.greet('tom'), 'hello tom');
+  });
+});
