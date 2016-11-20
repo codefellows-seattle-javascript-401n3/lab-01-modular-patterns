bde3fa4b3aec63e4d6a0a015ba5b01a8afa28115
Trenton Kress <trenton.kress@gmail.com>
2016-11-20 09:52:04 -0800

    Fixed the lint command. Annoying to remember to make that argument an array.

--- lab-trent/gulpfile.js
+++ lab-trent/gulpfile.js
@@ -5,7 +5,7 @@ const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
 
 gulp.task('lint', function() {
-  return gulp.src('**/*.js', '!node_modules/**')
+  return gulp.src(['**/*.js', '!node_modules/**'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
