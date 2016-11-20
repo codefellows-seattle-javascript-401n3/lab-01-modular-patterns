2c6064fde9a96f5202ede71e0db9b4942b86e647
Geoffrey Cox <gcox3d@gmail.com>
2016-11-20 10:45:43 -0800

    updated gulp watch task.

--- lab-geoff/gulpfile.js
+++ lab-geoff/gulpfile.js
@@ -4,20 +4,28 @@
 
 let gulp = require('gulp');
 
+//npm run useGulp
 gulp.task('default', function() {
   console.log('gulp default task')
 });
 
-// gulp.src('**/*.js').pipe()
-
+//npm run gulpLint
 gulp.task('lint', function() {
   console.log('run eslint')
+  // let stream = gulp.src('/*.js')
+  // console.log(stream)
 });
 
+//npm run gulpTest
 gulp.task('mocha', function() {
   console.log('run mocha')
 });
 
+//npm run gulpWatch
 gulp.task('watch', function() {
-  console.log('Watch files. Run the lint and test tasks')
+  console.log('Watch files. Run the lint and test tasks');
+  let watchedFiles = gulp.watch('./*.js', ['lint', 'mocha']);
+  watchedFiles.on('change', function(e) {
+    console.log('some change happened')
+  })
 });
