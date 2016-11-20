32defdc16633f6b5d36788d0f901ba0676de59c3
spamalope01 <spamalope01@yahoo.com>
2016-11-20 10:50:29 -0800

    added all the updates to the lab work assignment.  need to proof it.

---
+++ lab-jonathan/gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let gulp = require('gulp');
+let eslint = require('gulp-eslint');
+let mocha = require('gulp-mocha');
+let sillyName = require('sillyname');
+
+gulp.task('eslintrc', function() {
+  return gulp.src(['**/*.js', '!node_module/**'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function(){
+  gulp.src('../test/*-test.js')
+  .pipe(mocha({reporter: 'list'}));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(['**/*.js', '!node_modules/**'], ['eslintrc','test']);
+});
+
+gulp.task('favorite', function(){
+  console.log('I now bequeath you: ' + sillyName());
+});
