8c4bf8fd18144b7790c03a19d3ff083970a3390a
spamalope01 <spamalope01@yahoo.com>
2016-11-20 07:40:22 -0800

    organized and added index.js

---
+++ lab-jonathan/test/greet-test.js
@@ -0,0 +1,24 @@
+var verify = require('./lib/greet.js');
+var assert = require('assert');
+
+
+
+
+describe('app should say hello and give the provided name', function (){
+  describe('greet', function(){
+    it('will say hello to a user', function() {
+      assert.strictEqual('hello Jonathan', verify('Jonathan'))
+    });
+  });
+});
+
+
+// bonus points below
+describe('process.argv array', function() {
+  describe('argv', function(){
+    it('will pass in the name given on the command line to process.argv', function() {
+      process.argv[1] = 'dave';
+      assert('greet', verify('dave'));
+    });
+  });
+});
