b2cecd2dad9afa28bbef2bc1448f25315e14f116
Kyle Winckler <kyle.winckler@gmail.com>
2016-11-20 09:33:53 -0800

    added gulp dependencies and created a test to verify gulp is working

---
+++ lab-kyle/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const gulp = require('gulp')
+const eslint = require('gulp-eslint')
+const mocha = require('gulp-mocha')
+
+gulp.task('test-gulp', function() {
+  console.log('successful test')
+})
