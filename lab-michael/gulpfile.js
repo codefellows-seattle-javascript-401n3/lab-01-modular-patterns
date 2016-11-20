0ae78cae8680c0ae8fd130f8686c620dbe9bd868
Michael Bishop <polishthebishop@gmail.com>
2016-11-20 10:48:17 -0800

    tasks added in gulpfile

--- lab-michael/gulpfile.js
+++ lab-michael/gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
 
-gulp.task('default', function() {
-  // Write something in here //
+
+gulp.task('welcome', function() {
+  console.log('Welcome and Good Day');
+  // gulp.src('/**/*.js');
 });
+
+gulp.task('tester', function() {
+  gulp.src('./test/test-*.js')
+  .pipe(mocha());
+});
+
+gulp.task('linter', function() {
+  gulp.src('/**/*.js')
+  .pipe(eslint());
+});
+
+// gulp.task('default', function() {
+//   console.log('Process is run by default');
+// });
+gulp.task('default', ['welcome', 'tester', 'linter']);
