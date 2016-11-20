57f2470444eb4af03ea1bd8d4668f3fab7cfe85a
Geoffrey Cox <gcox3d@gmail.com>
2016-11-20 10:09:34 -0800

    added lab directions as a comment in gulpfile.js

--- lab-geoff/gulpfile.js
+++ lab-geoff/gulpfile.js
@@ -5,3 +5,16 @@ let gulp = require('gulp');
 gulp.task('default', function() {
   console.log('gulp default task')
 });
+
+
+
+// Directions:
+//
+// add your own .gitignore
+// include the class eslint
+
+// Gulp Tasks
+// make a lint task for running eslint
+// make a test task for running mocha
+// make a dev task that watches your files and runs the lint and test tasks
+// make a default task that runs your favorite task
